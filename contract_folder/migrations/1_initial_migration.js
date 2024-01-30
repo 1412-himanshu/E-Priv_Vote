c09f21f03b177f4458829d5d5e4fc44b1ee9c57d
Himanshu Hatwar <85329713+1412-himanshu@users.noreply.github.com>
2024-01-30 23:30:50 +1100

    Delete contract_folder directory

--- contract_folder/migrations/1_initial_migration.js
+++
@@ -1,6 +0,0 @@
-var Voting = artifacts.require("./Voting.sol");
-
-module.exports = function(deployer, networks, accounts) {
-  console.log("accounts:",accounts);
-  deployer.deploy(Voting, "college president", {from: accounts[0]});
-};
